e1dfaa5a249c9eb187e5c3fc4badc0bc3cbc41df
vasileios.litsas <vasileios.litsas@gmail.com>
2020-02-20 14:28:30 +0200

    added display exercises screen

---
+++ controllers/ExercisesController.js
@@ -0,0 +1,36 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import {getAsyncStorageItem, setAsyncStorageItem} from '../AsyncStorage';
+
+const ExercisesController = {
+  GetExercises: async () => {
+    return await getAsyncStorageItem('Exercises');
+  },
+  // SaveWorkout: async Workout => {
+  //   const User = await getAsyncStorageItem('AdminUser');
+  //   const Workouts = User.Workouts;
+  //   Workout.id = Workouts[Workouts.length - 1].id + 1;
+  //   Workouts.push(Workout);
+  //   User.Workouts = Workouts;
+
+  //   try {
+  //     await AsyncStorage.setItem('AdminUser', JSON.stringify(User));
+  //     return Workouts;
+  //   } catch (error) {
+  //     return null;
+  //   }
+  // },
+  // DeleteWorkout: async workoutId => {
+  //   const User = await getAsyncStorageItem('AdminUser');
+  //   const Workouts = User.Workouts;
+  //   const _Workouts = Workouts.filter(workout => workout.id !== workoutId);
+  //   User.Workouts = _Workouts;
+
+  //   try {
+  //     await AsyncStorage.setItem('AdminUser', JSON.stringify(User));
+  //     return _Workouts;
+  //   } catch (error) {
+  //     return null;
+  //   }
+  // },
+};
+export default ExercisesController;
--- screens/DayRoutine.js
+++ screens/DayRoutine.js
@@ -7,27 +7,58 @@ import {
   ScrollView,
   View,
   TouchableWithoutFeedback,
+  ActivityIndicator,
 } from 'react-native';
 import {Colors} from '../Colors';
+import ExercisesController from '../controllers/ExercisesController';
 
 function DayRoutine({navigation}) {
   const [DayRoutine, setDayRoutine] = useState([]);
+  const [daysExercises, setdaysExercises] = useState([]);
 
   useEffect(() => {
     const _DayRoutine = navigation.getParam('DayRoutine');
-    console.log('Routine: ', _DayRoutine);
     setDayRoutine(_DayRoutine);
+
+    getDaysExercises(_DayRoutine.ExerciseIds);
+
     return () => {
       //
     };
   }, []);
 
+  const getDaysExercises = async exerciseIds => {
+    const Exercises = await ExercisesController.GetExercises();
+
+    console.log('exs ', Exercises);
+    const daysExercises = Exercises.filter(exercise =>
+      exerciseIds.includes(exercise.Id),
+    );
+    console.log(daysExercises);
+    setdaysExercises(daysExercises);
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.container}>
-        <ScrollView keyExtractor={item => `${item.id}`}>
-          <Text>Day Routine</Text>
+        <ScrollView keyExtractor={item => `${item.Id}`}>
+          {daysExercises.length ? (
+            daysExercises.map(item => (
+              <TouchableWithoutFeedback
+                key={`${item.Id}`}
+                // onPress={() => goToWorkoutSchedule(item)}
+              >
+                <View style={styles.item}>
+                  <Text style={styles.Name}>{item.Name}</Text>
+                </View>
+              </TouchableWithoutFeedback>
+            ))
+          ) : (
+            <View style={{marginTop: 100}}>
+              <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+          )}
         </ScrollView>
       </SafeAreaView>
     </>
